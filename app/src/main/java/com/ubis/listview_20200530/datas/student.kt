390529dd853d97b1b1b6a291de851e117a020ece
nagasee0526 <roronalove@gmail.com>
2020-05-30 14:01:28 +0900

    X<:에 리스트뷰 배치 + 목록에 뿌려줄 데이터 클래스 생서ㅗㅇ

---
+++ app/src/main/java/com/ubis/listview_20200530/datas/student.kt
@@ -0,0 +1,8 @@
+package com.ubis.listview_20200530.datas
+
+class student {
+
+    val name = "미입력" // 이름이 기본적으로  string 가 되도록
+    val birthyear = 0 // 생년은 기본적으로  int 가 되도혹
+    val isMale = true // 성별을 boolean으로
+}
