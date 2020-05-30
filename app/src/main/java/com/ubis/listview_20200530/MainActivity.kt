c3038e841d959142ba2f44078e9f2b4021fb346c
nagasee0526 <roronalove@gmail.com>
2020-05-30 15:19:34 +0900

    학생목록 추가

--- app/src/main/java/com/ubis/listview_20200530/MainActivity.kt
+++ app/src/main/java/com/ubis/listview_20200530/MainActivity.kt
@@ -2,11 +2,21 @@ package com.ubis.listview_20200530
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ubis.listview_20200530.datas.student
 
 class MainActivity : AppCompatActivity() {
+
+    val students = ArrayList<student>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        students.add(student(inputname = "이현호", iby = 1981, gneder = true))
+        students.add(student(inputname = "김광철", iby = 1966, gneder = true))
+        students.add(student(inputname = "김재환", iby = 1993, gneder = true))
+        students.add(student(inputname = "박수정", iby = 1994, gneder = false))
+        students.add(student(inputname = "엄곤지", iby = 1984, gneder = false))
+
     }
 }
--- app/src/main/java/com/ubis/listview_20200530/datas/student.kt
+++ app/src/main/java/com/ubis/listview_20200530/datas/student.kt
@@ -1,8 +1,8 @@
 package com.ubis.listview_20200530.datas
 
-class student {
+class student(inputname : String, iby : Int, gneder : Boolean) {
 
-    val name = "미입력" // 이름이 기본적으로  string 가 되도록
-    val birthyear = 0 // 생년은 기본적으로  int 가 되도혹
-    val isMale = true // 성별을 boolean으로
+    val name = inputname // 이름이 기본적으로  string 가 되도록
+    val birthyear = iby // 생년은 기본적으로  int 가 되도혹
+    val isMale = Boolean // 성별을 boolean으로
 }
